fd1672d577406af315fa7c2762dac6290b102f3c
Hélio Júnior <heliorpjunior116@gmail.com>
2023-06-02 18:40:37 -0300

    first commit

---
+++ films.js
@@ -0,0 +1,91 @@
+let currentPageUrl = 'https://swapi.dev/api/films/'
+
+window.onload = async () => {
+    try {
+        await loadFilms(currentPageUrl)
+    } catch (error) {
+        console.log(error)
+        alert ('Erro ao carregar os cards')
+    }
+}
+
+async function loadFilms(url) {
+    const mainContent = document.getElementById('main-content')
+    mainContent.innerHTML = ''
+
+    try {
+
+        const response = await fetch(url)
+        const responseJson = await response.json()
+
+        responseJson.results.forEach((films) => {
+            const card = document.createElement("div")
+            card.style.backgroundImage = 
+            `url('https://starwars-visualguide.com/assets/img/films/${films.url.replace(/\D/g, "")}.jpg')`
+            card.className = "cards"
+
+            const filmsNameBG = document.createElement("div")
+            filmsNameBG.className = "films-name-bg"
+
+            const filmsName = document.createElement("span")
+            filmsName.className = "films-name"
+            filmsName.innerText = `${films.title}`
+
+            filmsNameBG.appendChild(filmsName)
+            card.appendChild(filmsNameBG)
+
+            card.onclick = () => {
+                const modal = document.getElementById("modal")
+                modal.style.visibility = "visible"
+
+                const modalContent = document.getElementById("modal-content")
+                modalContent.innerHTML = ''
+
+                const filmsImage = document.createElement("div")
+                filmsImage.style.backgroundImage = 
+                `url('https://starwars-visualguide.com/assets/img/films/${films.url.replace(/\D/g, "")}.jpg')`
+                filmsImage.className = "films-image"
+
+                const title = document.createElement("span")
+                title.className = "films-details"
+                title.innerText = `Titulo: ${films.title}`
+
+                const episode = document.createElement("span")
+                episode.className = "films-details"
+                episode.innerText = `Episodio: ${films.episode_id}`
+
+                const producer = document.createElement("span")
+                producer.className = "films-details"
+                producer.innerText = `Produtor: ${films.producer}`
+
+                const director = document.createElement("span")
+                director.className = "films-details"
+                director.innerText = `Diretor: ${films.director}`
+
+                const releaseDate = document.createElement("span")
+                releaseDate.className = "films-details"
+                releaseDate.innerText = `Lancamento: ${films.release_date}`
+
+                modalContent.appendChild(filmsImage)
+                modalContent.appendChild(title)
+                modalContent.appendChild(episode)
+                modalContent.appendChild(producer)
+                modalContent.appendChild(director)
+                modalContent.appendChild(releaseDate)
+            }
+
+            mainContent.appendChild(card)
+        })
+
+        currentPageUrl = url
+        
+    } catch (error) {
+        alert('Erro ao carregar os filmes')
+        console.log(error)
+    }
+}
+
+function hideModal() {
+    const modal = document.getElementById("modal")
+    modal.style.visibility = "hidden"
+}
